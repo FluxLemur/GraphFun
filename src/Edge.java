b3897e9a16ab71b66e3338b2278f46acb4d2961d
Leo Mehr <ljm262@cornell.edu>
2015-10-04 15:21:55 -0700

    2 space indents in Edge and Utils

--- src/Edge.java
+++ src/Edge.java
@@ -2,60 +2,60 @@
  * An Edge connects two Node objects.
  */
 public class Edge implements Comparable<Object> {
-    public final Node from;
-    public final Node to;
-    public final double length;
-    private double weight;
+  public final Node from;
+  public final Node to;
+  public final double length;
+  private double weight;
 
-    public Edge(Node f, Node t) {
-        from = f;
-        to = t;
-        weight = 1;
-        length = Node.getDistance(from, to);
-    }
+  public Edge(Node f, Node t) {
+    from = f;
+    to = t;
+    weight = 1;
+    length = Node.getDistance(from, to);
+  }
 
-    /**
-     * Both Nodes point to this edge.
-     */
-    public void addNodes() {
-        from.addEdge(this);
-        to.addEdge(this);
-    }
+  /**
+   * Both Nodes point to this edge.
+   */
+  public void addNodes() {
+    from.addEdge(this);
+    to.addEdge(this);
+  }
 
-    /** Whether this edge contains Node n*/
-    public boolean hasNode(Node n) {
-    	return n == from || n == to;
-    }
+  /** Whether this edge contains Node n*/
+  public boolean hasNode(Node n) {
+    return n == from || n == to;
+  }
 
-    public double getWeightedLength() {
-        return weight * length;
-    }
+  public double getWeightedLength() {
+    return weight * length;
+  }
 
-    public int compareTo(Object o) {
-        double cmp = length - ((Edge) o).length;
-        if (Math.abs(cmp) < 0.000000000001)
-            return 0;
-        if (cmp > 0)
-            return 1;
-        return -1;
-    }
+  public int compareTo(Object o) {
+    double cmp = length - ((Edge) o).length;
+    if (Math.abs(cmp) < 0.000000000001)
+      return 0;
+    if (cmp > 0)
+      return 1;
+    return -1;
+  }
 
-    /**
-     * Returns the Node of this edge that != in
-     * null if != from or to
-     * @param in
-     * @return
-     */
-    public Node getOther(Node in) {
-        if (in == from)
-            return to;
-        if (in == to)
-            return from;
-        return null;
-    }
+  /**
+   * Returns the Node of this edge that != in
+   * null if != from or to
+   * @param in
+   * @return
+   */
+  public Node getOther(Node in) {
+    if (in == from)
+      return to;
+    if (in == to)
+      return from;
+    return null;
+  }
 
-    @Override
-    public String toString() {
-        return "["+from+", "+to+"]";
-    }
+  @Override
+  public String toString() {
+    return "["+from+", "+to+"]";
+  }
 }
--- src/Utils.java
+++ src/Utils.java
@@ -2,21 +2,21 @@ import java.util.Random;
 
 
 public class Utils {
-    /**
-     * Generates list of nodes randomly picked between x: [0, width] and y: [0, height]
-     * @param num Number of nodes to generate
-     */
-    public static Node[] genPoints(int num, int width, int height) {
-        Random gen = new Random();
-        Node[] ps = new Node[num];
-        for (int i=0; i<num; i++) {
-            ps[i] = new Node(gen.nextInt(width), gen.nextInt(height));
-        }
-
-        return ps;
+  /**
+   * Generates list of nodes randomly picked between x: [0, width] and y: [0, height]
+   * @param num Number of nodes to generate
+   */
+  public static Node[] genPoints(int num, int width, int height) {
+    Random gen = new Random();
+    Node[] ps = new Node[num];
+    for (int i=0; i<num; i++) {
+      ps[i] = new Node(gen.nextInt(width), gen.nextInt(height));
     }
 
-    public static int scaledPointSize(int num_points, int width, int height) {
-    	return (int) Math.min(GraphDisplay.MAX_RADIUS, Math.sqrt(0.5*width*height/num_points));
-    }
+    return ps;
+  }
+
+  public static int scaledPointSize(int num_points, int width, int height) {
+    return (int) Math.min(GraphDisplay.MAX_RADIUS, Math.sqrt(0.5*width*height/num_points));
+  }
 }
